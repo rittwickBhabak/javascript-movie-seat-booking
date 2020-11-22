33729340e27f75a47ebd8ec114d18195093dbfe6
Rittwick <rittwick2019@gmail.com>
2020-11-22 11:12:50 +0530

    Populating UI with saved data from loalstorage done and thus the project is complete

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
 // Save selected movie index and price
 function setMovieData(movieIndex, moviePrice){
     localStorage.setItem('selectedMovieIndex', movieIndex);
@@ -16,12 +18,29 @@ function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-    localStorage.setItem('seats', JSON.stringify(seatsIndex));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = ticketPrice * selectedSeatsCount
+}
+
+// Get the saved data from localStorage and populate UI
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats!==null && selectedSeats.length>0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index)>-1){
+                seat.classList.add('selected')
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex!==null){
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
 }
 
 // Movie select event
@@ -38,3 +57,6 @@ container.addEventListener('click', e => {
         updateSelectedCount();
     }
 })
+
+// Initial count and total set
+updateSelectedCount();
